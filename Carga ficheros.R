fa4a28fa62b0dc57bbe41344bbfed52130a54a53
maria <mariaepelde8@gmail.com>
2020-04-24 09:00:14 +0200

    ñado los ficheros originales

---
+++ Carga ficheros.R
@@ -0,0 +1,5 @@
+
+setwd("C:/Users/auror/Desktop/RETOS/RETO 3/DS/Ejercicio GIT")
+read.csv("time_series_19-covid-Confirmed.csv")
+read.csv("time_series_19-covid-Deaths.csv")
+read.csv("time_series_19-covid-Recovered.csv")
