369b65c504e05bc06098e0b9ec4f7de05ea3a121
maria <mariaepelde8@gmail.com>
2020-04-24 09:09:07 +0200

    Edito y renombro el Programa.R

--- Carga ficheros.R
+++
@@ -1,5 +0,0 @@
-
-setwd("C:/Users/auror/Desktop/RETOS/RETO 3/DS/Ejercicio GIT")
-read.csv("time_series_19-covid-Confirmed.csv")
-read.csv("time_series_19-covid-Deaths.csv")
-read.csv("time_series_19-covid-Recovered.csv")
